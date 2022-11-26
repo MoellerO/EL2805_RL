6f393dd9744122b2e39d93dff14d81b79c3a186f
MoellerO <oliver.moeller@die-olchies.de>
2022-11-26 18:08:23 +0100

    update

--- problem2/problem2.py
+++ problem2/problem2.py
@@ -31,19 +31,27 @@ low, high = env.observation_space.low, env.observation_space.high
 random.seed(0)
 np.random.seed(0)
 # Parameters
-N_episodes = 500      # Number of episodes to run for training
+N_episodes = 50      # Number of episodes to run for training
 discount_factor = 1.   # Value of gamma
 LAMBDA = 0.
-ALPHA = 0.2
+# ALPHA = 0.01 # comment in if using decay
+ALPHA = 0.06
+ALPHA_MIN = 0.0000001
 M = 0.2
 P = 2  # Order of fourier base
 ETA = np.array([[i, j] for i in range(P + 1) for j in range(P + 1)])
-ETA = np.delete(ETA, (0), axis=0)  # remove [0,0]
+# ETA = np.delete(ETA, (0), axis=0)  # remove [0,0]
 
+# SCOPE is used for decaying alpha (number of elemtns used to compute the current mean reward)
+SCOPE = 3
 EPS = 1.
-w = np.zeros((ETA.shape[0], k))  # init weights
+Q_w = np.ones((k)) * 200
+print(Q_w)
+
+# w = np.zeros((ETA.shape[0], k))  # init weights
+w = np.ones((ETA.shape[0], k)) * 200  # init weights
 v = np.zeros((ETA.shape[0], k))  # init weights
-Q_w = np.zeros((k))
+
 
 # Reward
 episode_reward_list = []  # Used to save episodes reward
@@ -89,110 +97,155 @@ def scale_alpha(alpha):
     return alpha / eta_normalitzation
 
 
-def decay_alpha(alpha):
-    if total_episode_reward > -130:
-        if alpha > 0.000001:
-            alpha *= 0.8
-    elif total_episode_reward < -160:
-        if alpha < 0.1:
-            alpha = alpha / 0.8
-    print(total_episode_reward)
-    return scale_alpha(alpha)
-
-    # if i % 10 == 0:
-    #     alpha = alpha * 0.9
-    # if total_episode_reward > -100:
-    #     alpha *= 1 - 0.2 * (total_episode_reward > -100)
-    # elif total_episode_reward > -130:
-    #     alpha *= 1 - 0.1 * (total_episode_reward > -130)
-    # elif total_episode_reward > -150:
-    #     alpha *= 1 - 0.01 * (total_episode_reward > -150)
+def decay_alpha(episode, alpha, recent_reward_list):
+    if episode > 0 and episode % SCOPE == 0:
+        mean_recent_reward = np.mean(recent_reward_list)
+        if mean_recent_reward > -100:
+            alpha *= 0.5
+        elif mean_recent_reward > -110:
+            alpha *= 0.5
+        elif mean_recent_reward > -120:
+            alpha *= 0.5
+        elif mean_recent_reward > -125:
+            alpha *= 0.7
+        print('Alpha:', alpha)
+        print('Reward:', mean_recent_reward)
+    return alpha
 
 
-print(phi(scale_state_variables(env.reset())))
+REPETITIONS = 1
 
-if 1:
-    alpha = scale_alpha(ALPHA)
-    eps = EPS
-    # Training process
-    for i in range(N_episodes):
-        # Reset enviroment data
-        done = False
-        total_episode_reward = 0.
+final_rewards = np.zeros(REPETITIONS)
+alphas = []
+lambdas = []
+avg_total_rewards = np.zeros(REPETITIONS)
+for rep in range(REPETITIONS):
+    print('Repitition:', rep, '/', REPETITIONS)
+    if 1:
+        # reset parameters
+        Q_w = np.zeros((k))
+        w = np.zeros((ETA.shape[0], k))  # init weights
+        alpha = ALPHA
+        alpha = scale_alpha(alpha)
+        eps = EPS
 
-        # init state and action
-        state = scale_state_variables(env.reset())
-        a_t = choose_action(eps, Q_w)
+        # comment in to plot alpha - value
+        # alpha = rep / (5 * REPETITIONS)
+        # alphas.append(alpha)
 
-        # reset eligibility trace and velocity term used for momentum
-        z = np.zeros((ETA.shape[0], k))
-        v = np.zeros((ETA.shape[0], k))
-        while not done:
-            # env.action_space.n tells you the number of actions
-            # available
+        # comment in to plot lambda - value
+        # LAMBDA = rep / (REPETITIONS)
+        # lambdas.append(LAMBDA)
+        # alpha = 0.06
 
-            # Get next state and reward.  The done variable
-            # will be True if you reached the goal position,
-            # False otherwise
-            next_state, reward, done, _ = env.step(a_t)
-            next_state = scale_state_variables(next_state)
-            action_next = choose_action(0, Q_w)
+        # Training process
+        last_n_rewards = np.zeros(SCOPE)
+        for i in range(N_episodes):
+            # Reset enviroment data
+            done = False
+            total_episode_reward = 0.
 
-            # Update episode reward
-            total_episode_reward += reward
-            delta_t = reward + discount_factor * \
-                np.dot(w[:, action_next].T, phi(next_state)) - \
-                np.dot(w[:, a_t].T, phi(state))
+            # init state and action
+            state = scale_state_variables(env.reset())
+            a_t = choose_action(eps, Q_w)
 
-            # Update z
-            for a in range(k):
-                if a == a_t:
-                    # note that the derivative of our approx. function w_a^T*phi(s) is simply phi(s)
-                    z[:, a] = discount_factor * LAMBDA * z[:, a] + phi(state)
-                else:
-                    z[:, a] = discount_factor * LAMBDA * z[:, a]
+            # reset eligibility trace and velocity term used for momentum
+            z = np.zeros((ETA.shape[0], k))
+            v = np.zeros((ETA.shape[0], k))
+            while not done:
+              # Get next state and reward.
+                next_state, reward, done, _ = env.step(a_t)
+                next_state = scale_state_variables(next_state)
+                action_next = choose_action(0, Q_w)
 
-            z = np.clip(z, -5, 5)
+                # Update episode reward
+                total_episode_reward += reward
+                delta_t = reward + discount_factor * \
+                    np.dot(w[:, action_next].T, phi(next_state)) - \
+                    np.dot(w[:, a_t].T, phi(state))
 
-            # Update w with SGD with Nesterov Acceleration
-            v = M * v + alpha * delta_t * z
-            w = w + M * v + alpha * delta_t * z
-            # w = w + v # only momentum
+                # Update z
+                for a in range(k):
+                    if a == a_t:
+                        # note that the derivative of our approx. function w_a^T*phi(s) is simply phi(s)
+                        z[:, a] = discount_factor * \
+                            LAMBDA * z[:, a] + phi(state)
+                    else:
+                        z[:, a] = discount_factor * LAMBDA * z[:, a]
 
-            # Update Q_w
-            Q_w = np.dot(w.T, phi(state))
+                z = np.clip(z, -5, 5)
 
-            # Update state for next iteration
-            state = next_state
-            a_t = action_next
+                # Update w with SGD with Nesterov Acceleration
+                v = M * v + alpha * delta_t * z
+                w = w + M * v + alpha * delta_t * z
+                # w = w + v # only momentum
 
-        alpha = decay_alpha(alpha)
-        print(i, 'alpha', alpha)
-        # Append episode reward
-        episode_reward_list.append(total_episode_reward)
-        if eps > 0.01:
-            eps = eps * 0.99
+                # Update Q_w
+                Q_w = np.dot(w.T, phi(state))
+
+                # Update state for next iteration
+                state = next_state
+                a_t = action_next
+
+            last_n_rewards[i % SCOPE] = total_episode_reward
+            # alpha = decay_alpha(i, alpha, last_n_rewards)
+            # alpha = alpha * 0.95 # linear decay
+            # Append episode reward
+            episode_reward_list.append(total_episode_reward)
+            if eps > 0.00001:
+                eps = eps * 0.97
+        final_rewards[rep] = total_episode_reward  # add reward of last episode
+        avg_total_rewards[rep] = np.mean(episode_reward_list)
 
         # Close environment
         env.close()
-    with open('weights.pkl', 'wb') as handle:
-        N = ETA
-        data = {'W': w.T, 'N': N}
-        pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        if 0:
+            # save weights
+            with open('weights.pkl', 'wb') as handle:
+                N = ETA
+                data = {'W': w.T, 'N': N}
+                pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        if 1:
+            # Plot Rewards
+            plt.plot([i for i in range(1, N_episodes + 1)],
+                     episode_reward_list, label='Episode reward')
+            plt.plot([i for i in range(1, N_episodes + 1)],
+                     running_average(episode_reward_list, 10), label='Average episode reward')
+            plt.xlabel('Episodes')
+            plt.ylabel('Total reward')
+            plt.title('Total Reward vs Episodes')
+            plt.legend()
+            plt.grid(alpha=0.3)
+            plt.show()
 
+print('Final Rewards:', final_rewards)
+print('Final Mean Reward:', np.mean(final_rewards))
+print('Final Std Reward:', np.std(final_rewards))
+if 0:
+    # plot rewards by alpha/gamma
     # Plot Rewards
-    plt.plot([i for i in range(1, N_episodes + 1)],
-             episode_reward_list, label='Episode reward')
-    plt.plot([i for i in range(1, N_episodes + 1)],
-             running_average(episode_reward_list, 10), label='Average episode reward')
-    plt.xlabel('Episodes')
+    ci = 1.96 * np.std(avg_total_rewards) / np.sqrt(len(lambdas))
+    # ci = 1.96 * np.std(avg_total_rewards) / np.sqrt(len(alphas))
+
+    fig, ax = plt.subplots()
+    ax.plot(lambdas, avg_total_rewards)
+    ax.fill_between(lambdas, (avg_total_rewards - ci),
+                    (avg_total_rewards + ci), color='b', alpha=.1)
+    # ax.plot(alphas, avg_total_rewards)
+    # ax.fill_between(alphas, (avg_total_rewards - ci),
+    #                 (avg_total_rewards + ci), color='b', alpha=.1)
+    # plt.plot(alphas)
+    # plt.xlabel('Alpha')
+    plt.xlabel('Lambda')
     plt.ylabel('Total reward')
-    plt.title('Total Reward vs Episodes')
+    # plt.title('Total Reward vs Alpha')
+    plt.title('Total Reward vs Lambda')
     plt.legend()
     plt.grid(alpha=0.3)
     plt.show()
 
-if 1:
+if 0:
+    # load weights and create 3D plots
     with open('weights.pkl', 'rb') as f:
         data = pickle.load(f)
     w = data['W'].T
